f11725de3e9e2e7fd5d406f9872843b105335586
viishal1627 <kumarvishal1627@gmail.com>
2025-09-05 21:12:50 +0530

    Add files via upload
    Added my portfolio website files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple animation for waving hand in Hero section
+const wave = document.querySelector('.wave');
+if (wave) {
+    setInterval(() => {
+        wave.classList.toggle('wave-animate');
+    }, 1000);
+}
