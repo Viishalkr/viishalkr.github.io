8ee425c1bbbb8fdd180b4deb4df5b535db679816
viishal1627 <kumarvishal1627@gmail.com>
2025-12-30 21:47:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-// no JS needed for static hero
+function toggleMenu() {
+  const menu = document.getElementById("mobileMenu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}// no JS needed for static hero
